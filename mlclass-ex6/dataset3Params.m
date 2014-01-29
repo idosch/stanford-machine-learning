9690de8e33d1d785790b706a1960f97b084a0f06
Ido Schimmel <idoschimmel@gmail.com>
2014-01-29 16:59:32 +0200

    Finish second part - optimal values for C and sigma.

--- mlclass-ex6/dataset3Params.m
+++ mlclass-ex6/dataset3Params.m
@@ -22,13 +22,27 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+%C = sigma = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+%C_final = sigma_final = 0.01;
+%min_err = exp(100); % Some large value.
+%for i = 1 : length(C)
+%    for j = 1 : length(C)
+%        model= svmTrain(X, y, C(i), @(x1, x2) gaussianKernel(x1, x2, sigma(j)));
+%        predictions = svmPredict(model, Xval);
+%        err = mean(double(predictions ~= yval));
+%        if (err < min_err)
+%            C_final = C(i);
+%            sigma_final = sigma(j);
+%            min_err = err;
+%        end
+%    end
+%end
+%
+%C = C_final;
+%sigma = sigma_final;
 
-
-
-
-
-
-
+C = 1;
+sigma = 0.1;
 % =========================================================================
 
 end
